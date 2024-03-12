6daec8184516978da8ff723c127ec5e6e5f5dee7
gwyn <gwynethmojados@gmail.com>
2024-03-12 14:39:52 -0400

    Initial commit

---
+++ src/main/java/firstpush/Main.java
@@ -0,0 +1,13 @@
+package firstpush;
+
+public class Main {
+    public static void main(String[] args) {
+        String Product = "Rice";
+        int quantity = 20;
+        double ricePrice = 5.0;
+        double milesfee = 5.0;
+        // miles fee for 10mi = $5
+
+        System.out.println("It will cost the user" + " " + quantity*ricePrice + milesfee);
+    }
+}
