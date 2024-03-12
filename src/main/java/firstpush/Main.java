4a2304828ca084a6a66f181a87706f09f80c50ae
gwyn <gwynethmojados@gmail.com>
2024-03-12 17:53:07 -0400

    Input Scanner

--- src/main/java/firstpush/Main.java
+++ src/main/java/firstpush/Main.java
@@ -1,7 +1,39 @@
 package firstpush;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the product?");
+        String WhatProduct = scan.next();
+        System.out.println("What Quantity of" + " " + WhatProduct);
+        int quantity = scan.nextInt();
+        System.out.println("How many miles are you from headquarters?");
+        int distance = scan.nextInt();
+        int fee1 = 2;
+        int fee2 = 5;
+        int fee3 = 10;
+        int fee4 = 15;
+        int fee5 = 20;
+        double ProductPrice = 5.0;
+        if (distance >= 0 && distance <=4) {
+            System.out.println("Your total including delivery will be " + fee1 + ProductPrice * quantity);
+        }else if (distance >= 5 && distance <= 15) {
+            System.out.println("Your total including delivery will be " + fee2 + ProductPrice * quantity);
+        }else if (distance >= 16 && distance <= 25) {
+            System.out.println("Your total including delivery will be " + fee3 + ProductPrice * quantity);
+        }else if (distance >= 26 && distance <= 50) {
+            System.out.println("Your total including delivery will be " + fee4 + ProductPrice * quantity);
+        } else {
+            System.out.println("Your total including delivery will be " + fee5 + ProductPrice * quantity); }
+
+
+
+        /*
+
+
         String Product = "Rice";
         int quantity = 20;
         double ricePrice = 5.0;
@@ -27,18 +59,13 @@ public class Main {
         } else {
             System.out.println(fee5 + ricePrice * quantity); } // Remember curly braces
 
+         */
 
 
 
 
-            // int age = 31;
-            //
-            //        if (age > 18) {
-            //            System.out.println("You can party with us at YearUP");
-            //        } else if (age == 18) {
-            //            System.out.println("Can also party with us");
-            //        }else{
-            //            System.out.println("Too young to party");
+
+
 
 
 
