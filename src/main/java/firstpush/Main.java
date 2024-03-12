a04396b19337abc54a9a8094100c30d4e724ff09
gwyn <gwynethmojados@gmail.com>
2024-03-12 15:07:56 -0400

    CodeChange

--- src/main/java/firstpush/Main.java
+++ src/main/java/firstpush/Main.java
@@ -5,9 +5,40 @@ public class Main {
         String Product = "Rice";
         int quantity = 20;
         double ricePrice = 5.0;
-        double milesfee = 5.0;
-        // miles fee for 10mi = $5
+        double distance = 17;
 
-        System.out.println("It will cost the user" + " " + quantity*ricePrice + milesfee);
-    }
-}
+        int fee1 = 2;
+        int fee2 = 5;
+        int fee3 = 10;
+        int fee4 = 15;
+
+
+
+        if (distance <= 4) {
+            System.out.println(fee1 +ricePrice + quantity);
+
+        }else if (distance <= 15) {
+            System.out.println(fee2 + ricePrice * quantity);
+        }if (distance <= 25) {
+            System.out.println(fee3 + ricePrice * quantity);
+        }else if (distance > 26) {
+            System.out.println(fee4 + ricePrice * quantity);
+        }
+
+
+
+
+            // int age = 31;
+            //
+            //        if (age > 18) {
+            //            System.out.println("You can party with us at YearUP");
+            //        } else if (age == 18) {
+            //            System.out.println("Can also party with us");
+            //        }else{
+            //            System.out.println("Too young to party");
+
+
+
+
+    } // end method
+} // end class
